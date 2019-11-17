d1ae149ad5e945712f6b373c1b5d3a14934a68f3
Mouzzam Tahir <moazzamtahir81@gmail.com>
2019-11-17 15:43:31 +0500

    Whole project is completed

--- Day 16 /Project 1/Project 1/ViewController.swift
+++ Day 16 /Project 1/Project 1/ViewController.swift
@@ -7,14 +7,83 @@
 //
 
 import UIKit
+import WebKit //to use WKWebView
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WKNavigationDelegate {
 
+    @IBOutlet var webView: WKWebView!
+    
+    var urlString = "https://www.apple.com"
+    var progressBar: UIProgressView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        title = "APPLE"
+        
+        progressBar = UIProgressView(progressViewStyle: .default)
+        progressBar.sizeToFit()
+        progressBar.progressTintColor = .red
+        
+        let spacer = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: self, action: nil)
+        let refresh = UIBarButtonItem(barButtonSystemItem: .refresh, target: self, action: #selector(reload))
+        let progress = UIBarButtonItem(customView: progressBar)
+        
+        
+        
+        toolbarItems = [refresh, spacer, progress].reversed()
+        navigationController?.setToolbarItems(toolbarItems, animated: true)
+        navigationController?.setToolbarHidden(false, animated: true)
+        
+        webView.addObserver(self, forKeyPath: #keyPath(WKWebView.estimatedProgress), options: .new, context: nil)
+        
+        webView.navigationDelegate = self
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Choose", style: .done, target: self, action: #selector(choseWeb))
+        
+       loadUrl()
     }
-
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "estimatedProgress" {
+            progressBar.progress = Float(webView.estimatedProgress)
+        }
+    }
+    
+    @objc func reload() {
+        webView.reload()
+    }
+    
+    func loadUrl() {
+        guard let url = URL(string: urlString) else {
+            print("URL not valid")
+            return
+        }
+        
+        webView.load(URLRequest(url: url))
+    }
+    
+    @objc func choseWeb() {
+        let alert = UIAlertController(title: "Choose page", message: nil, preferredStyle: .actionSheet)
+        let action1 = UIAlertAction(title: "apple.com", style: .default, handler: openPage)
+        let action2 = UIAlertAction(title: "google.com", style: .default, handler: openPage)
+        let action3 = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alert.addAction(action1)
+        alert.addAction(action2)
+        alert.addAction(action3)
+        //trezgl alert.popoverPresentationController?.barButtonItem
+        present(alert, animated: true)
+    }
+    
+    @objc func openPage(_ sender: UIAlertAction) {
+        urlString = "https://" + (sender.title ?? "google.com")
+        print(urlString)
+        loadUrl()
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
+    }
 
 }
 
